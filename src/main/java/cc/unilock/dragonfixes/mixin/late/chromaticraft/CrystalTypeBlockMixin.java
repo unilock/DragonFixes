be7f2ecbd5cd1135f89f8c229b1ba75ae6f5789f
unilock <katherines1337@outlook.com>
2026-05-20 03:09:51 -0400

    more metadata checks in CrystalTypeBlock

--- src/main/java/cc/unilock/dragonfixes/mixin/late/chromaticraft/CrystalTypeBlockMixin.java
+++ src/main/java/cc/unilock/dragonfixes/mixin/late/chromaticraft/CrystalTypeBlockMixin.java
@@ -16,4 +16,22 @@ public class CrystalTypeBlockMixin {
         }
         return original;
     }
+
+    @ModifyExpressionValue(method = "getLightValue", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/IBlockAccess;getBlockMetadata(III)I"))
+    private int getLightValue(int original) {
+        if (original > 15) {
+            DragonFixes.LOGGER.debug("Attempted to get light value of CrystalTypeBlock with metadata > 15");
+            return original % 16;
+        }
+        return original;
+    }
+
+    @ModifyExpressionValue(method = "getCrystalElement", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/IBlockAccess;getBlockMetadata(III)I"))
+    private int getCrystalElement(int original) {
+        if (original > 15) {
+            DragonFixes.LOGGER.debug("Attempted to get crystal element of CrystalTypeBlock with metadata > 15");
+            return original % 16;
+        }
+        return original;
+    }
 }
