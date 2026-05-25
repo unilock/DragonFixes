c338ff85021f767f189a2196ce4b7231515f824a
unilock <katherines1337@outlook.com>
2026-05-25 18:20:52 -0400

    slightly better impl of blasted bedrock breaking

--- src/main/java/cc/unilock/dragonfixes/mixin/late/rotarycraft/TileEntityBedrockBreakerMixin.java
+++ src/main/java/cc/unilock/dragonfixes/mixin/late/rotarycraft/TileEntityBedrockBreakerMixin.java
@@ -2,20 +2,16 @@ package cc.unilock.dragonfixes.mixin.late.rotarycraft;
 
 import Reika.DragonAPI.ModInteract.ItemHandlers.FactorizationHandler;
 import Reika.RotaryCraft.TileEntities.Production.TileEntityBedrockBreaker;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.Block;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = TileEntityBedrockBreaker.class, remap = false)
 public class TileEntityBedrockBreakerMixin {
-    @Inject(method = "isBedrock", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/World;getBlock(III)Lnet/minecraft/block/Block;"), cancellable = true)
-    private void isBedrock$factorizationBlastedBedrock(World world, int x, int y, int z, CallbackInfoReturnable<Boolean> cir, @Local Block id) {
-        if (id == FactorizationHandler.getInstance().bedrockID2) {
-            cir.setReturnValue(true);
-        }
+    @ModifyReturnValue(method = "isBedrock", at = @At("RETURN"))
+    private boolean isBedrock$factorizationBlastedBedrock(boolean original, @Local Block id) {
+        return original || id == FactorizationHandler.getInstance().bedrockID2;
     }
 }
