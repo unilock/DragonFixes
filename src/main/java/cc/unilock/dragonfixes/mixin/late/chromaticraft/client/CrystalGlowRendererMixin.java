1bc6ea57292abc479c1afe6d308a1aca68c33735
unilock <katherines1337@outlook.com>
2026-05-25 05:07:36 -0400

    actually fix another crystal glow crash

--- src/main/java/cc/unilock/dragonfixes/mixin/late/chromaticraft/client/CrystalGlowRendererMixin.java
+++ src/main/java/cc/unilock/dragonfixes/mixin/late/chromaticraft/client/CrystalGlowRendererMixin.java
@@ -1,17 +1,23 @@
 package cc.unilock.dragonfixes.mixin.late.chromaticraft.client;
 
 import Reika.ChromatiCraft.Render.ISBRH.CrystalGlowRenderer;
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(value = CrystalGlowRenderer.class, remap = false)
 public class CrystalGlowRendererMixin {
-    @ModifyConstant(method = "renderInventoryBlock", constant = @Constant(intValue = 16, ordinal = 0))
-    private int renderInventoryBlock(int constant) {
-        return 6;
+    @Definition(id = "metadata", local = @Local(type = int.class, argsOnly = true, ordinal = 0))
+    @Expression("metadata / 16")
+    @ModifyExpressionValue(method = "renderInventoryBlock", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private int renderInventoryBlock(int original) {
+        if (original > 6) {
+            return original % 6;
+        }
+        return original;
     }
 
     @ModifyExpressionValue(method = "renderWorldBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/IBlockAccess;getBlockMetadata(III)I", remap = true))
