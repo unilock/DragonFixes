f2b8b52f9112e6a2d91243ec3c7618c3b3098816
unilock <katherines1337@outlook.com>
2026-05-25 05:00:57 -0400

    fix another crystal glow crash

--- src/main/java/cc/unilock/dragonfixes/mixin/late/chromaticraft/client/CrystalGlowRendererMixin.java
+++ src/main/java/cc/unilock/dragonfixes/mixin/late/chromaticraft/client/CrystalGlowRendererMixin.java
@@ -4,9 +4,16 @@ import Reika.ChromatiCraft.Render.ISBRH.CrystalGlowRenderer;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(value = CrystalGlowRenderer.class, remap = false)
 public class CrystalGlowRendererMixin {
+    @ModifyConstant(method = "renderInventoryBlock", constant = @Constant(intValue = 16, ordinal = 0))
+    private int renderInventoryBlock(int constant) {
+        return 6;
+    }
+
     @ModifyExpressionValue(method = "renderWorldBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/IBlockAccess;getBlockMetadata(III)I", remap = true))
     private int renderWorldBlock(int original) {
         if (original > 15) {
