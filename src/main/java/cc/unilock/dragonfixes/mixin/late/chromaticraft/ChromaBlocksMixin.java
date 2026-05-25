ab3e0236ee61548f11bc446424f4e05874c63a3b
unilock <katherines1337@outlook.com>
2026-05-25 05:16:23 -0400

    fix yet another crystal glow crash

---
+++ src/main/java/cc/unilock/dragonfixes/mixin/late/chromaticraft/ChromaBlocksMixin.java
@@ -0,0 +1,22 @@
+package cc.unilock.dragonfixes.mixin.late.chromaticraft;
+
+import Reika.ChromatiCraft.Registry.ChromaBlocks;
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(value = ChromaBlocks.class, remap = false)
+public class ChromaBlocksMixin {
+    @Definition(id = "meta", local = @Local(type = int.class, argsOnly = true))
+    @Expression("meta / 16")
+    @ModifyExpressionValue(method = "getMultiValuedName", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private int renderInventoryBlock(int original) {
+        if (original > 6) {
+            return original % 6;
+        }
+        return original;
+    }
+}
